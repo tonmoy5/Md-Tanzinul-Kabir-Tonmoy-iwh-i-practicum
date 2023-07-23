b1920d5e5a2b0045c688f873ba0c0a3fd2f8ce7d
tonmoy5 <tonmoy52532@gmail.com>
2023-07-23 13:10:26 +0600

    Readme.md updated, Private api added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-3eba7ac1-cebf-42a9-8c15-07153b77fad2';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
